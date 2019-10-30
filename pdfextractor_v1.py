99f7cab742564413009e8529d557cffa2bf806af
dpak <dhspak@gmail.com>
2019-10-30 15:31:02 -0400

    initial commit

---
+++ pdfextractor_v1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Title: pdfextractor.py
+Authoer: Daniel Pak
+Date: 10-30-2019
+Description: THis is a pdf extraction script.
+"""
+
+
+import pandas as pd
+import numpy as np
+import pdb
+
+df = pd.DataFrame(np.random.randn(6, 4), columns=list('ABCD'))
+
+#pdb.set_trace()
+
+print("Khalid is learning data science")
